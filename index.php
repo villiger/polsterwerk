04181146fe77ad8d6a7edcba1a35476367c4ced2
Andy Villiger <villiger@starmind.com>
2014-05-19 11:55:08 +0200

    new google analytics tracking

--- index.php
+++ index.php
@@ -40,15 +40,13 @@ $page = isset($_GET['page']) && !empty($_GET['page']) ? $_GET['page'] : $default
 		</nav>
         <?php include('pages/' . $page . '.php'); ?>
 	</div>
-	<script type="text/javascript">
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-44731667-1']);
-		_gaq.push(['_trackPageview']);
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-	</script>
+	<script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-44731667-1', 'polsterwerk.ch');
+        ga('send', 'pageview');
+    </script>
 </body>
 </html>
