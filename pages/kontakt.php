4555bedcde604dca86ada563689d4feefec2fee4
Andy Villiger <a.villiger@gmail.com>
2019-01-28 15:51:42 +0100

    Update kontakt.php

--- pages/kontakt.php
+++ pages/kontakt.php
@@ -16,7 +16,7 @@
         <strong>Wie Sie uns finden:</strong> Wir sind in Niederrohrdorf zentral gelegen, vis-à-vis der Einfahrt zum Coop Parkplatz.
     </p>
 
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2699.4141834315215!2d8.30220295153074!3d47.4233666085083!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479013a82639099f%3A0x8a796a68e196371e!2sPolsterwerk!5e0!3m2!1sde!2sch!4v1548686942491" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe><br/>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2699.4141834315215!2d8.30220295153074!3d47.4233666085083!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479013a82639099f%3A0x8a796a68e196371e!2sPolsterwerk!5e0!3m2!1sde!2sch!4v1548686942491" width="100%" height="300" frameborder="0" style="border:1px solid black;" allowfullscreen></iframe><br/>
     <small>
         <a target="_blank" rel="nofollow" href="https://www.google.ch/maps?sll=47.40898200000001,8.406278900000004&amp;sspn=0.29047692243409334,0.4766169944823576&amp;t=m&amp;q=Holzr%C3%BCtistrasse+2,+Niederrohrdorf&amp;ie=UTF8&amp;hq=&amp;hnear=Holzr%C3%BCtistrasse+2,+5443+Niederrohrdorf,+Aargau&amp;z=14&amp;iwloc=A&amp;ll=47.423404,8.304375&amp;source=embed" style="text-align:left">Größere Kartenansicht</a>
     </small>
