ddd5293d90b71a06aba6b8608c3fc878680b7ad4
Andy Villiger <a.villiger@gmail.com>
2019-01-28 12:03:47 +0100

    added instafeed

--- pages/galerie.php
+++ pages/galerie.php
@@ -1,41 +1,30 @@
 <div class="content">
 	<div class="gallery">
-		<?php
-
-		require_once(__DIR__ . '/../lib/wideimage/WideImage.php');
-
-		$imageDir = __DIR__ . '/../img/galerie/';
-		$cacheDir = __DIR__ . '/../cache/';
-
-		$images = array();
-		$files = scandir($imageDir);
-        rsort($files);
-
-		foreach ($files as $file) {
-			if (! preg_match('/^\./', $file)) {
-				$sourcePath = $imageDir . $file;
-				$hash = sha1($file);
-				$thumbName = "{$hash}_thumb.jpg";
-				$imageName = "{$hash}.jpg";
-		
-				if (! file_exists($cacheDir . $thumbName)) {
-					$image = WideImage::load($sourcePath);
-			
-					$image->resize(600, 600, 'outside')
-						->saveToFile($cacheDir . $imageName);
-			
-					$image->resize(420, 420, 'outside')
-						->autoCrop()
-						->crop('center', 'center', 420, 420)
-						->saveToFile($cacheDir . $thumbName);
-				}
-
-                echo "<a href=\"/cache/$imageName\" data-lightbox=\"galerie\">";
-				echo "<img src=\"/cache/$thumbName\" alt=\"Galerie\">";
-                echo "</a>";
-			}
-		}
-		?>
+		<div id="instafeed" class="instafeed"></div>
         <div class="clear"></div>
 	</div>
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/instafeed.js/1.4.1/instafeed.min.js"></script>
+<script type="text/javascript">
+	var userFeed = new Instafeed({
+		get: 'user',
+		userId: '2231736641',
+		accessToken: '2231736641.1677ed0.df6b0200364642d3a060c53322348cda',
+		resolution: 'low_resolution',
+		sortBy: 'random',
+		limit: 16,
+		template: '<a href="{{link}}" data-lightbox="galerie" id="{{id}}" style="background-image: url({{image}});"></a>'
+	});
+	userFeed.run();
+</script>
+<style>
+	.instafeed a {
+		float: left;
+		margin: 20px;
+		width: 200px;
+		height: 200px;
+		background-size: cover;
+		background-repeat: none;
+		background-position: center center;
+	}
+</style>
