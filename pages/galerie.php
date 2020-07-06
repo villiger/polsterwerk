8b2d7f0e8cf7c14a03b2001aced3f2caedafe9d0
Andy Villiger <a.villiger@gmail.com>
2020-07-06 13:04:32 +0200

    Update galerie.php

--- pages/galerie.php
+++ pages/galerie.php
@@ -1,38 +1,8 @@
 <div class="content">
-	<div class="gallery">
-		<div id="instafeed" class="instafeed"></div>
-        <div class="clear"></div>
-		<div style="margin: 10px; margin-top: 20px;">
-			<a href="https://www.instagram.com/polsterwerk/" target="_blank">Mehr Bilder auf Instagram</a>
+	<div class="gallery" style="margin: 0 -20px;">
+		<iframe src="https://snapwidget.com/embed/847319" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:460px; height:2760px;"></iframe>
+		<div style="margin-top: 20px;">
+			<a href="https://www.instagram.com/polsterwerk/" target="_blank">Hier Klicken für mehr Bilder</a>
 		</div>
 	</div>
 </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/instafeed.js/1.4.1/instafeed.min.js"></script>
-<script type="text/javascript">
-	var userFeed = new Instafeed({
-		get: 'user',
-		userId: '2231736641',
-		accessToken: '2231736641.1677ed0.df6b0200364642d3a060c53322348cda',
-		resolution: 'standard_resolution',
-		limit: 30,
-		template: '<a href="{{image}}" data-lightbox="galerie" id="{{id}}" style="background-image: url({{image}});"></a>',
-		sortBy: 'most-recent'
-	});
-	userFeed.run();
-</script>
-<style>
-	.gallery {
-		margin-left: -30px;
-		margin-right: -30px;
-	}
-
-	.instafeed a {
-		float: left;
-		margin: 10px;
-		width: 220px;
-		height: 220px;
-		background-size: cover;
-		background-repeat: none;
-		background-position: center center;
-	}
-</style>
