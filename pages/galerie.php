c968d9011012285cc86ee579ba3a166d207762e3
Andy Villiger <a.villiger@gmail.com>
2020-07-06 13:10:24 +0200

    Update galerie.php

--- pages/galerie.php
+++ pages/galerie.php
@@ -1,6 +1,6 @@
 <div class="content">
 	<div class="gallery" style="margin: 0 -20px;">
-		<iframe src="https://snapwidget.com/embed/847319" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:460px; height:2760px;"></iframe>
+		<iframe src="//lightwidget.com/widgets/9a2f54d5ef6051c089816e719c34d601.html" scrolling="no" allowtransparency="true" style="width: 460px; height: 2760px; border: 0; overflow: hidden;"></iframe>
 		<div style="margin-top: 20px;">
 			<a href="https://www.instagram.com/polsterwerk/" target="_blank">Hier Klicken für mehr Bilder</a>
 		</div>
